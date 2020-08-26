9668b4335169a0df50642870aa71bfef6dfd0a14
oliver-89 <wjr980729@163.com>
2020-08-26 21:41:34 +0800

    [poker hands] [Eric&Oliver] feature:pass should_return_full_house_when_verify_poker_type_given_3D_3S_3H_6D_6C

--- src/main/java/com/pokerHands/PokerType.java
+++ src/main/java/com/pokerHands/PokerType.java
@@ -1,54 +1,85 @@
 package com.pokerHands;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class PokerType {
 
-  public String[] verifyPokerType(String[] poker) {
+  public int packPokerType(String[] poker) {
     Character[] pokersNumber = Arrays.stream(poker).map(s -> s.charAt(0)).toArray(Character[]::new);
-    String[] result = calcSamePokerNumber(pokersNumber);
-    if("3".equals(result[0])){
-      result[0] = "three of a kind";
-    }
-    if ("2".equals(result[0])) {
-      result[0] = "two pairs";
-    }
-    if ("1".equals(result[0])) {
-      result[0] = "pair";
-    }
-    return result;
+    Integer[] pokers = pokerToNumber(pokersNumber);
+    Integer[] result = calcPokerNumber(pokers);
+    int pokerType = verifyPokerType(result);
+    return pokerType;
   }
 
+  private Integer[] calcPokerNumber(Integer[] pokersNumber){
+    Integer[] poker = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    for (int i = 0; i < pokersNumber.length; i++) {
+      poker[pokersNumber[i] - 1] ++;
+    }
+    return poker;
+  }
 
-
-  public String[] calcSamePokerNumber(Character[] pokersNumber) {
-    char maxPairNumber = '0';
-    int repetitionNumber = 0;
-    String[] result = new String[2];
-    for (int i = 0; i < pokersNumber.length - 1; i++) {
-      Character temp = pokersNumber[i];
-      for (int j = i + 1; j < pokersNumber.length; j++) {
-        if (temp.equals(pokersNumber[j])) {
-          if (maxPairNumber < temp) {
-            result[1] = String.valueOf(temp);
-            maxPairNumber = temp;
-          }
-          repetitionNumber ++;
+  private int verifyPokerType(Integer[] pokersNumber) {
+    int maxSamePokerNumber = (int) Collections.max(Arrays.asList(pokersNumber));
+    int pairPokerNumber = 0;
+    if (maxSamePokerNumber == 2) {
+      for (int i = 0; i < pokersNumber.length; i++) {
+        if (pokersNumber[i] == 2) {
+          pairPokerNumber ++;
         }
       }
+      return pairPokerNumber;
     }
-    result[0] = String.valueOf(repetitionNumber);
-    return result;
+    if (maxSamePokerNumber == 3) {
+      for (int i = 0; i < pokersNumber.length; i++) {
+        if (pokersNumber[i] == 2) {
+          return 6;
+        }
+      }
+      return maxSamePokerNumber;
+    }
+    if (maxSamePokerNumber == 4) {
+      return 7;
+    }
+    return pairPokerNumber;
   }
+
+  private Integer[] pokerToNumber(Character[] pokersNumber) {
+    return Arrays.stream(pokersNumber).map(pokerNumber -> {
+      if (pokerNumber == 'A') {
+        return 14;
+      }
+      if (pokerNumber == 'T') {
+        return 10;
+      }
+      if (pokerNumber == 'J') {
+        return 10;
+      }
+      if (pokerNumber == 'Q') {
+        return 10;
+      }
+      if (pokerNumber == 'K') {
+        return 10;
+      }
+      return Integer.parseInt(String.valueOf(pokerNumber));
+    }).toArray(Integer[]::new);
+  }
+}
 //
-//  public String[] verifyThreeNumber(Character[] pokersNumber) {
+//  public String[] calcPairPokerNumber(Character[] pokersNumber) {
+//    char maxPairNumber = '0';
 //    int repetitionNumber = 0;
 //    String[] result = new String[2];
 //    for (int i = 0; i < pokersNumber.length - 1; i++) {
 //      Character temp = pokersNumber[i];
 //      for (int j = i + 1; j < pokersNumber.length; j++) {
 //        if (temp.equals(pokersNumber[j])) {
-//          result[1] = String.valueOf(pokersNumber[j]);
+//          if (maxPairNumber < temp) {
+//            result[1] = String.valueOf(temp);
+//            maxPairNumber = temp;
+//          }
 //          repetitionNumber ++;
 //        }
 //      }
@@ -56,4 +87,18 @@ public class PokerType {
 //    result[0] = String.valueOf(repetitionNumber);
 //    return result;
 //  }
-}
+////
+////  public String[] verifyFourNumber(Character[] pokersNumber) {
+////    int repetitionNumber = 0;
+////    String[] result = new String[2];
+////    for (int i = 0; i < pokersNumber.length - 1; i++) {
+////      Character temp = pokersNumber[i];
+////        if (temp.equals(pokersNumber[j])) {
+////          result[1] = String.valueOf(pokersNumber[j]);
+////          repetitionNumber ++;
+////        }
+////    }
+////    result[0] = String.valueOf(repetitionNumber);
+////    return result;
+////  }
+
--- src/test/java/com/pokerHands/PokerTypeTest.java
+++ src/test/java/com/pokerHands/PokerTypeTest.java
@@ -9,43 +9,62 @@ public class PokerTypeTest {
   PokerType pokerType = new PokerType();
 
   @Test
-  void should_return_pair_and_K_when_verify_poker_type_given_KD_KS_2H_5D_9C() {
+  void should_return_pair_when_verify_poker_type_given_KD_KS_2H_5D_9C() {
     //given
     String[] poker = {"KD", "KS", "2H", "5D", "9C"};
 
     //when
-    String[] result = pokerType.verifyPokerType(poker);
+    int result = pokerType.packPokerType(poker);
 
     //then
-    assertEquals("pair",result[0]);
-    assertEquals("K",result[1]);
+    assertEquals(1,result);
   }
 
   @Test
-  void should_return_two_pairs_and_K_when_verify_poker_type_given_KD_KS_2H_2D_9C() {
+  void should_return_two_pairs_when_verify_poker_type_given_KD_KS_2H_2D_9C() {
     //given
     String[] poker = {"KD", "KS", "2H", "2D", "9C"};
 
     //when
-    String[] result = pokerType.verifyPokerType(poker);
+    int result = pokerType.packPokerType(poker);
 
     //then
-    assertEquals("two pairs",result[0]);
-    assertEquals("K",result[1]);
+    assertEquals(2,result);
   }
 
   @Test
-  void should_return_three_of_a_kind_and_K_when_verify_poker_type_given_KD_KS_KH_2D_9C() {
+  void should_return_three_of_a_kind_when_verify_poker_type_given_KD_KS_KH_2D_9C() {
     //given
     String[] poker = {"KD", "KS", "KH", "2D", "9C"};
 
     //when
-    String[] result = pokerType.verifyPokerType(poker);
+    int result = pokerType.packPokerType(poker);
 
     //then
-    assertEquals("three of a kind",result[0]);
-    assertEquals("K",result[1]);
+    assertEquals(3,result);
   }
 
+  @Test
+  void should_return_four_of_a_kind_when_verify_poker_type_given_KD_KS_KH_2D_KC() {
+    //given
+    String[] poker = {"KD", "KS", "KH", "2D", "KC"};
 
+    //when
+    int result = pokerType.packPokerType(poker);
+
+    //then
+    assertEquals(7,result);
+  }
+
+  @Test
+  void should_return_full_house_when_verify_poker_type_given_3D_3S_3H_6D_6C() {
+    //given
+    String[] poker = {"3D", "3S", "3H", "6D", "6C"};
+
+    //when
+    int result = pokerType.packPokerType(poker);
+
+    //then
+    assertEquals(6,result);
+  }
 }
